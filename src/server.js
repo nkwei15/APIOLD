9c786d8b3daea0ae5bd6a08b71be32e54ab4421b
nkwei15 <45564333+nkwei15@users.noreply.github.com>
2019-11-26 19:52:06 +0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -90,14 +90,14 @@ app.patch('/update/:postID', async (req,res)=>{
 });
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static('NewsWeatherApp/build'));
+    app.use(express.static(path.join(__dirname, 'build')));
 
     app.get('*', (req,res)=>{
-    res.sendFile(path.resolve(__dirname,"NewsWeatherApp","build","index.html"))
+    res.sendFile(path.resolve(__dirname,"build","index.html"))
     })
 
 }
-app.use('/feedback', feedbackRouter);
+
 
 //connect to MangoDB
 mongoose.connect('mongodb+srv://user_123:123@cluster0-txqzp.mongodb.net/test?retryWrites=true&w=majorityCo'
